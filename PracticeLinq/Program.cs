b3baeee1860d387e945356a343369f5ecc27569e
Jared Lewis <jlewis8493@gmail.com>
2022-01-28 17:59:08 -0500

    Linq Exercise 1 Complete

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameList = new List<string>()
+            {
+                "Mass Effect",
+                "KOTOR",
+                "Assasin's Creed",
+                "Halo",
+                "Doom2"
+            };
+
+            //strings are Arrays of characters (char)
+
+            //var orderedGameList = gameList.OrderBy(name => name.Length).ToList();
+
+            //foreach(var gameName in orderedGameList)
+            //{
+            //    Console.WriteLine(gameName);
+            //}
+
+            gameList.OrderBy(name => name.Length).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
